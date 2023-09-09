40cf0eadf6328f735865d18619af305dcab31d02
BUSOLA12 <111029122+BUSOLA12@users.noreply.github.com>
2023-09-09 14:00:23 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify
+from datetime import datetime
+from collections import OrderedDict
+
+app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False  # Disable sorting of JSON keys
+
+@app.route('/')
+def index():
+    # Get the query parameters
+    slack_name = request.args.get('slack_name')
+    track = request.args.get('track')
+
+    # Get the current day of the week
+    current_day = datetime.utcnow().strftime('%A')
+
+    # Get the current UTC time
+    utc_time = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+
+    # Create the response data as an OrderedDict
+    data = OrderedDict([
+        ('slack_name', slack_name),
+        ('current_day', current_day),
+        ('utc_time', utc_time),
+        ('track', track),
+        ('github_file_url', 'https://github.com/username/repo/blob/main/file_name.ext'),
+        ('github_repo_url', 'https://github.com/username/repo'),
+        ('status_code', 200)
+    ])
+
+    # Return the JSON response
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run()
